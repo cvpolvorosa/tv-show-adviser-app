6d3de962f03f424876d508106223d5e8537d6a3f
cvpolvorosa <cvpolvorosa@gmail.com>
2023-02-08 09:45:14 -0500

    Added Fetch Call using TMDB API

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -1,8 +1,11 @@
-export class TVShowAPI{
-    static async fetchPopulars(){
-        //req
+import axios from "axios";
 
-        //return
-        
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=99e999af977c58243859de72e31e412d";
+export class TVShowAPI {
+    static async fetchPopulars() {
+        const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`);
+        console.log(response.data.results)
+        return response.data.results;
     }
 }

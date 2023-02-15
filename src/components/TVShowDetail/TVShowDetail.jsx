3414c1c5d2ec06cfe085c01eaac14ceb09d1dae9
cvpolvorosa <cvpolvorosa@gmail.com>
2023-02-15 02:22:26 -0500

    Implemented a five star rating component

--- src/components/TVShowDetail/TVShowDetail.jsx
+++ src/components/TVShowDetail/TVShowDetail.jsx
@@ -1,9 +1,13 @@
 import s from "./style.module.css"
-
-export function TVShowDetail({tvShow}) {
+import { FiveStarRating } from "../FiveStarRating/FiveStarRating"
+export function TVShowDetail({ tvShow }) {
+    const rating = tvShow.vote_average / 2;
     return <div>
         <div className={s.title}>{tvShow.name}</div>
-        <div className={s.rating}>{tvShow.vote_average / 2}/5</div>
+        <div className={s.rating_container}>
+            <FiveStarRating rating={rating} />
+            <span className={s.rating}>{rating}/5</span>
+        </div>
         <div className={s.overview}>{tvShow.overview}</div>
     </div>
 }
